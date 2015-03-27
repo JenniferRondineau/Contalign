87495facc0e3a235b455bc428be7c20cc11ac08c
jenniferRondineau <jennifer.rondineau@etu.univ-nantes.fr>
2015-03-27 14:47:29 +0100

    20150327

--- src/countalign.c
+++ src/countalign.c
@@ -32,8 +32,8 @@ THE SOFTWARE.
 #include "debug.h"
 #include <errno.h>  
 
-
 #define VERIFY_NOT_NULL(POINTER) do{if(POINTER==NULL) { fprintf(stderr,"Memory Alloc failed File %s Line%d.\n",__FILE__,__LINE__); exit(EXIT_FAILURE);}}while(0)
+
 
 typedef struct SampleName
  	{
@@ -78,8 +78,6 @@ printf(" Version : v.1 \n");
 
 
 
-
-
 static int compareGroup(const void *g1, const void *g2)
 {
 	Group *group1 = (Group *) g1;
@@ -90,76 +88,92 @@ static int compareGroup(const void *g1, const void *g2)
 
 
 
+
 int main(int argc, char** argv)
 	{
-	FILE* file=NULL;	
+	FILE* file=NULL;
+	FILE* file_fastq=NULL;
 	char* filename_out =NULL; 
-	int c;
+	int c, i=0, sample_count=0, group_count=0, case_o =0;
 	char* output_report; 
 	bam_hdr_t *header=NULL;
 	bam1_t *b = NULL;
 	b = bam_init1();
 	long nReads=0;
-	int n=0, i=0;
-	char *rgId=NULL, *sampleName=NULL;	
+	const char *rgId=NULL, *sampleName=NULL;	
 	Sample* samples=NULL;
-	int sample_count=0;
 	Group* group=NULL;
-	int group_count=0;
 	uint8_t *search = NULL; 
 	char* Groupsearch = NULL;
+	uint8_t* seq= NULL;
+        uint8_t* qual = NULL;
+	char *qname = NULL;
+	char *nbRead = NULL;
+	int8_t *buf = NULL;
+	int8_t *buf2 = NULL;
+	int32_t qlen =NULL;
 
 	
 	// get commande line options
 	 while ((c = getopt(argc, argv,"o:O:hv")) != -1) {
-	 switch(c)
-		 {
-		 case 'h': 
-		 	{
-		 	usage();
-		 	return EXIT_SUCCESS;
-		 	break;
-		 	}
-		 case 'v':
-		 	{
-		 	version();
-		 	return EXIT_SUCCESS;
-		 	break;
-		 	}
-		 
-		 case 'o': output_report = strdup(optarg); break;
-		 case 'O' :
-		 	{
-		 	filename_out = strdup(optarg);
-		 	if( filename_out == NULL )
-		 		{
-		 		fprintf(stderr,"Cannot alloc memory.\n");
-		 		return EXIT_FAILURE;
-		 		}
-		 	break;
-		 	}
-		case '?': 
+		 switch(c)
+			 {
+			 case 'h': 
+			 	{
+			 	usage();
+			 	return EXIT_SUCCESS;
+			 	break;
+			 	}
+			 case 'v':
+			 	{
+			 	version();
+			 	return EXIT_SUCCESS;
+			 	break;
+			 	}
+			 
+			 case 'o': 
+			 	{
+			 	output_report = strdup(optarg); 
+			 	case_o = 1;
+			 	break;
+			 	}
+			 case 'O' :
+			 	{
+			 	filename_out = strdup(optarg);
+			 	if( filename_out == NULL )
+			 		{
+			 		fprintf(stderr,"Cannot alloc memory.\n");
+			 		return EXIT_FAILURE;
+			 		}
+			 	break;
+			 	}
+			case '?': 
+				{
+				 fprintf(stderr, "ERROR: option -%c is undefined\n", optopt);
+			 	 return EXIT_FAILURE;
+			 	 break;
+			 	 }
+			case ':': 
+				{
+				fprintf(stderr, "ERROR: option -%c requires an argument\n",optopt);
+				return EXIT_FAILURE;
+			 	break;
+			 	}
+			default :
+				{
+				usage(); 
+				return EXIT_FAILURE;
+				break;
+				}
+			 }
+		if (case_o == 0)
 			{
-			 fprintf(stderr, "ERROR: option -%c is undefined\n", optopt);
-		 	 return EXIT_FAILURE;
-		 	 break;
-		 	 }
-		case ':': 
-			{
-			fprintf(stderr, "ERROR: option -%c requires an argument\n",optopt);
+			fprintf(stderr, "ERROR\n");
+			usage();
 			return EXIT_FAILURE;
-		 	break;
-		 	}
-		default :
-			{
-			usage(); 
-			return EXIT_FAILURE;
-			break;
-			}
-		 }
-	
+			}	
 
-	 } // si ./ countalign aljsd 
+	 }
 
 	
 	
@@ -187,8 +201,7 @@ int main(int argc, char** argv)
 			fprintf(stderr,"Failed to open output file . %s.\n",strerror(errno));
 			return EXIT_FAILURE;
 			} 
-
-
+			
 
 	void *iter = sam_header_parse2(header->text);
 	while ( (iter = sam_header2key_val(iter, "RG","ID","SM",&rgId,&sampleName) )!=NULL)
@@ -221,16 +234,13 @@ int main(int argc, char** argv)
 
 			 /* group must be unique */							
 			
-			for (i=0;i<group_count;++i)
-			{ 
-				if (strcmp (group[i].rgId,rgId) == 0)
-				{
-				fprintf(stderr,"DUP GROUP");
-				return EXIT_FAILURE;
+			for (i=0;i<group_count;++i) { 
+				if (strcmp (group[i].rgId,rgId) == 0) {
+					fprintf(stderr,"DUP GROUP");
+					return EXIT_FAILURE;
 				}
 			}
-					
-								
+										
 			group = realloc(group,sizeof(Group)*(group_count+1));	
 			VERIFY_NOT_NULL(group);	
 			group[group_count].rgId = strdup(rgId);
@@ -238,9 +248,6 @@ int main(int argc, char** argv)
 			
 			group[group_count].sample = searchSample;  
 			group_count++;							
-			
-
-			
 		}
 		
 
@@ -251,48 +258,72 @@ int main(int argc, char** argv)
 		}
 
 
-	qsort( group, group_count, sizeof(Group), compareGroup);
-
-
-	samfile_t *temp_bam = samopen("temp.bam", "wb", header );
-	
-		if (temp_bam == NULL) 
-			{
-			fprintf(stderr,"Failed to open output file . %s.\n",strerror(errno));
-			return EXIT_FAILURE;
-			} 
-
-	
 	DEBUG;
+	
+	qsort( group, group_count, sizeof(Group), compareGroup);
+	
+	file_fastq=fopen("temp.fastq","a");
+	
 	while(sam_read1(fp, header, b) >= 0)
 	{ 
 		nReads++;
 		if ( (b->core.flag & BAM_FUNMAP ) )
 			{
-				
+			//Write read name	
 			search = bam_aux_get(b, "RG");
-			Groupsearch = bam_aux2Z(search);
+			Groupsearch = bam_aux2Z(search); 
 			
-			struct Group key, *res;
-			key.rgId = Groupsearch;
+			struct Group *res;
 			res = bsearch(&Groupsearch, group, group_count, sizeof(Group), compareGroup);
-			if (res == NULL)
-				{
+			if (res == NULL) {
 				fprintf(stderr, "%s unknown read group\n", Groupsearch);
 				return EXIT_FAILURE;
-				}
-			else 
+			} else 
 				{
-				res->sample->unMap++;
+				res->sample->unMap++; // Count unmapped reads by sample
 				}
-				
-			samwrite(temp_bam, b); 	
+			
+			// Write sequence
+			qlen = b->core.l_qseq;	
+       			buf=(int8_t*)malloc(qlen*sizeof(int8_t));       			
+       			for (i = 0; i < qlen; ++i) buf[i] = 0;
+       			seq = bam_get_seq(b);
+        		for (i = 0; i < qlen; ++i){
+           			buf[i] = bam_seqi(seq, i);
+           			buf[i] = seq_nt16_str[buf[i]];
+           			}
+           		buf[qlen] = '\0';
+
+			// Write sequence name
+			qname = strdup(bam_get_qname(b));
+			
+			if ((b->core.flag & BAM_FREAD1) && !(b->core.flag & BAM_FREAD2)) nbRead="/1";
+         		else if ((b->core.flag & BAM_FREAD2) && !(b->core.flag & BAM_FREAD1)) nbRead="/2";
+           		
+           		
+           		// Write quality	
+           		qual = bam_get_qual(b);	
+           		buf2=(int8_t*)malloc(qlen*sizeof(int8_t));       			
+       			for (i = 0; i < qlen; ++i) buf2[i] = 0;
+       			for (i = 0; i < qlen; ++i) buf2[i] = 33 + qual[i];
+       			buf2[qlen] = '\0';		
+			
+			// Write fastq with unmapped reads
+			if ( file_fastq != NULL) {
+ 				fprintf(file_fastq, "@%s%s\n%s\n+\n%s\n", qname, nbRead, (char*)buf, (char*)buf2);
+ 			} else 
+ 				{
+ 				fprintf(stderr,"Cannot read file. %s.\n",strerror(errno));
+ 				return EXIT_FAILURE;
+ 				}
+			
+
 			}
-		samwrite(out_file, b); 
+		samwrite(out_file, b); // Write read in the output file
 	}
 	
 	
-
+	// Write the report containing number of unmapped reads by sample
  	file=fopen(output_report,"w"); 
  		if ( file != NULL) 
  		{
@@ -307,22 +338,26 @@ int main(int argc, char** argv)
 	      	 	return EXIT_FAILURE;
       	 		}     	 		
       	 		
+      	
       	 		
-      	DEBUG;	 		
+      	DEBUG;	
+        
+        // Close files, free and return
         fclose(file);
 	bam_destroy1(b);
-	sam_close(fp); 
+	sam_close(fp);
 	samclose(out_file);
-	samclose(temp_bam);
 	free(filename_out);
-
-
+	free(file_fastq);
+	free(buf);
+	free(buf2);
 	
 	for (i=0; i<group_count; i++)
 		{
 		free(group[i].rgId);
 		}
 	free(group);
+
 
 	
 	for (i=0; i<sample_count; i++)
@@ -331,6 +366,7 @@ int main(int argc, char** argv)
 		}
 	free(samples);
 	
+	
 
 	
         return EXIT_SUCCESS;
@@ -338,6 +374,5 @@ int main(int argc, char** argv)
         
         
         
-        // ensuite possible de transformer du BAM en FASTQ par cette commande : samtools bam2fq test.bam > test.fastq
         // le fichier .fastq va pouvoir servir pour faire les alignements avec les réferences des contaminants.
         
